c4b9089d74a44cdcfe320894bcdc13ae344300ea
Kiara <kiaraymg@gmail.com>
2021-07-23 19:08:16 +0200

    Stage 1. Added 2 functions to increment/decrement the counter's value.

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const data = [
+  { id: 1, value: 0 },
+  { id: 2, value: 0 },
+  { id: 3, value: 0 },
+];
+
+function Counter(props: { value: number }) {
+  let { value } = props;
+  const [count, setCount] = useState(value);
+
+  const incrementCounter = (newValue: number) => {
+    setCount((prev) => (prev += newValue));
+  };
+
+  const decrementCounter = (newValue: number) => {
+    setCount((prev) => (prev += newValue));
+  };
+
+  return (
+    <div>
+      <b>{count}</b>
+      <div>
+        <button onClick={() => decrementCounter(-1)}>-</button>
+        <button onClick={() => incrementCounter(1)}>+</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      {data.map((counter) => (
+        <Counter key={counter.id} value={counter.value} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
